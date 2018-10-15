73cddf87a3e97c3ae1c8ffddc5d5f564ee88d6bd
62karolina <62karolina@mail.ru>
2018-10-15 22:15:06 +0300

    верстка
    Верстка коробок и дизайн страниц

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -24,136 +24,19 @@
                                         <div class="box__bg box__box box__bg_gray"><img src="/build/img/box__gray-box.png" alt="" class="box__bg-img"/></div>
                                     </div>
                                 </div>
-                                <div class="box__header-wrapper">
-                                    <div class="box__header-line">
-                                        <div class="box__volume">
-                                            <div class="box__volume-text">Коробка</div>
-                                            <div class="box__volume-sum">№1</div>
-                                        </div>
-                                        <div class="box__cost">{{$case1->price}}<span class="rouble">p</span></div>
-                                    </div>
-                                </div>
-                                <div class="box__img-wrapper"><img src="http://214010.selcdn.ru/ranbox/box/box1.png" alt="Коробка №1" title="Коробка №1" class="box__img"/></div>
+
+                                <div class="box__img-wrapper"><img src="http://214010.selcdn.ru/ranbox/box/box1.png"  class="box__img"/></div>
                                 <div class="box__button-wrapper">
                                     <button class="box__button">Открыть коробку</button>
                                 </div>
                             </div>
-                            <div class="col-md-8 col-lg-8 hidden-xs hidden-sm">
-                                <div class="box__item_header">Коробка содержит {{$c1}} товаров:</div>
-                                @foreach($item1 as $item)
-                                    <div class="box__item box__item_gray" >
-                                        <div class="box__item-glow"></div>
-                                        <img src="{{$item->image}}" class="box__item-img" style=" width: 70px;height: 70px;"/>
-                                    </div>
-                                @endforeach
-                            </div>
+
                         </div>
 
                     </div>
                 </a>
             </div>
 
-
-
-            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                <a href="/box/2">
-                    <div class="box box_green-box">
-                        <div class="row">
-                            <div class="col-md-4 col-lg-4 col-xs-12 col-sm-12">
-                                <div class="box__bg-layout">
-                                    <div class="box__bg-wrapper">
-                                        <div class="box__bg box__bg_shadow"><img src="/build/img/box__shadow.png" alt="" class="box__bg-img"/></div>
-                                        <div class="box__bg box__bg_hov box__bg_red"><img src="/build/img/box__red-hov.png" alt="" class="box__bg-img"/></div>
-                                        <div class="box__bg box__box box__bg_red"><img src="/build/img/box__red-box.png" alt="" class="box__bg-img"/></div>
-                                        <div class="box__bg box__bg_hov box__bg_orange"><img src="/build/img/box__orange-hov.png" alt="" class="box__bg-img"/></div>
-                                        <div class="box__bg box__box box__bg_orange"><img src="/build/img/box__orange-box.png" alt="" class="box__bg-img"/></div>
-                                        <div class="box__bg box__bg_hov box__bg_blue"><img src="/build/img/box__blue-hov.png" alt="" class="box__bg-img"/></div>
-                                        <div class="box__bg box__box box__bg_blue"><img src="/build/img/box__blue-box.png" alt="" class="box__bg-img"/></div>
-                                        <div class="box__bg box__bg_hov box__bg_green"><img src="/build/img/box__green-hov.png" alt="" class="box__bg-img"/></div>
-                                        <div class="box__bg box__box box__bg_green"><img src="/build/img/box__green-box.png" alt="" class="box__bg-img"/></div>
-                                        <div class="box__bg box__bg_hov box__bg_gray"><img src="/build/img/box__gray-hov.png" alt="" class="box__bg-img"/></div>
-                                        <div class="box__bg box__box box__bg_gray"><img src="/build/img/box__gray-box.png" alt="" class="box__bg-img"/></div>
-                                    </div>
-                                </div>
-                                <div class="box__header-wrapper">
-                                    <div class="box__header-line">
-                                        <div class="box__volume">
-                                            <div class="box__volume-text">Коробка</div>
-                                            <div class="box__volume-sum">№2</div>
-                                        </div>
-                                        <div class="box__cost">{{$case2->price}}<span class="rouble">p</span></div>
-                                    </div>
-                                </div>
-                                <div class="box__img-wrapper"><img src="http://214010.selcdn.ru/ranbox/box/box2.png" alt="Коробка №2" title="Коробка №2" class="box__img"/></div>
-                                <div class="box__button-wrapper">
-                                    <button class="box__button">Открыть коробку</button>
-                                </div>
-                            </div>
-                            <div class="col-md-8 col-lg-8 hidden-xs hidden-sm">
-                                <div class="box__item_header">Коробка содержит {{$c2}} товаров:</div>
-                                @foreach($item2 as $item)
-                                    <div class="box__item box__item_green">
-                                        <div class="box__item-glow"></div>
-                                        <img src="{{$item->image}}" class="box__item-img" style=" width: 70px;height: 70px;"/>
-                                    </div>
-                                @endforeach
-                            </div>
-                        </div>
-
-                    </div>
-                </a>
-            </div>
-
-
-
-            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                <a href="/box/3">
-                    <div class="box box_red-box">
-                        <div class="row">
-                            <div class="col-md-4 col-lg-4 col-xs-12 col-sm-12">
-                                <div class="box__bg-layout">
-                                    <div class="box__bg-wrapper">
-                                        <div class="box__bg box__bg_shadow"><img src="/build/img/box__shadow.png" alt="" class="box__bg-img"/></div>
-                                        <div class="box__bg box__bg_hov box__bg_red"><img src="/build/img/box__red-hov.png" alt="" class="box__bg-img"/></div>
-                                        <div class="box__bg box__box box__bg_red"><img src="/build/img/box__red-box.png" alt="" class="box__bg-img"/></div>
-                                        <div class="box__bg box__bg_hov box__bg_orange"><img src="/build/img/box__orange-hov.png" alt="" class="box__bg-img"/></div>
-                                        <div class="box__bg box__box box__bg_orange"><img src="/build/img/box__orange-box.png" alt="" class="box__bg-img"/></div>
-                                        <div class="box__bg box__bg_hov box__bg_blue"><img src="/build/img/box__blue-hov.png" alt="" class="box__bg-img"/></div>
-                                        <div class="box__bg box__box box__bg_blue"><img src="/build/img/box__blue-box.png" alt="" class="box__bg-img"/></div>
-                                        <div class="box__bg box__bg_hov box__bg_green"><img src="/build/img/box__green-hov.png" alt="" class="box__bg-img"/></div>
-                                        <div class="box__bg box__box box__bg_green"><img src="/build/img/box__green-box.png" alt="" class="box__bg-img"/></div>
-                                        <div class="box__bg box__bg_hov box__bg_gray"><img src="/build/img/box__gray-hov.png" alt="" class="box__bg-img"/></div>
-                                        <div class="box__bg box__box box__bg_gray"><img src="/build/img/box__gray-box.png" alt="" class="box__bg-img"/></div>
-                                    </div>
-                                </div>
-                                <div class="box__header-wrapper">
-                                    <div class="box__header-line">
-                                        <div class="box__volume">
-                                            <div class="box__volume-text">Коробка</div>
-                                            <div class="box__volume-sum">№3</div>
-                                        </div>
-                                        <div class="box__cost">{{$case3->price}}<span class="rouble">p</span></div>
-                                    </div>
-                                </div>
-                                <div class="box__img-wrapper"><img src="http://214010.selcdn.ru/ranbox/box/box3.png" alt="Коробка №3" title="Коробка №3" class="box__img"/></div>
-                                <div class="box__button-wrapper">
-                                    <button class="box__button">Открыть коробку</button>
-                                </div>
-                            </div>
-                            <div class="col-md-8 col-lg-8 hidden-xs hidden-sm">
-                                <div class="box__item_header">Коробка содержит {{$c3}} товаров:</div>
-                                @foreach($item3 as $item)
-                                    <div class="box__item box__item_red">
-                                        <div class="box__item-glow"></div>
-                                        <img src="{{$item->image}}" class="box__item-img" style=" width: 70px;height: 70px;" />
-                                    </div>
-                                @endforeach
-                            </div>
-                        </div>
-
-                    </div>
-                </a>
-            </div>
         </div>
     </div>
 </div>
