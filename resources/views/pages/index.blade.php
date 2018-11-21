77daab6a202678b0de42fc2278068cb8ed4759b2
WowWo <kurlovich1998@gmail.com>
2018-11-21 23:15:25 +0300

    верстка кейсов и вещей

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -12,18 +12,14 @@
                             <div class="col-md-4 col-lg-4 col-xs-12 col-sm-12">
                                 <div class="box__bg-layout">
                                     <div class="box__bg-wrapper">
-                                        <img  src="{{$case->image}}" alt="{{$case->name}}" title="" class="box__img">
-                                        <div class="box__bg box__bg_shadow"><img src="/build/img/box__shadow.png" alt="" class="box__bg-img"/></div>
-                                        <div class="box__bg box__bg_hov box__bg_red"><img src="/build/img/box__red-hov.png" alt="" class="box__bg-img"/></div>
-                                        <div class="box__bg box__box box__bg_red"><img src="/build/img/box__red-box.png" alt="" class="box__bg-img"/></div>
-                                        <div class="box__bg box__bg_hov box__bg_orange"><img src="/build/img/box__orange-hov.png" alt="" class="box__bg-img"/></div>
-                                        <div class="box__bg box__box box__bg_orange"><img src="/build/img/box__orange-box.png" alt="" class="box__bg-img"/></div>
-                                        <div class="box__bg box__bg_hov box__bg_blue"><img src="/build/img/box__blue-hov.png" alt="" class="box__bg-img"/></div>
-                                        <div class="box__bg box__box box__bg_blue"><img src="/build/img/box__blue-box.png" alt="" class="box__bg-img"/></div>
-                                        <div class="box__bg box__bg_hov box__bg_green"><img src="/build/img/box__green-hov.png" alt="" class="box__bg-img"/></div>
-                                        <div class="box__bg box__box box__bg_green"><img src="/build/img/box__green-box.png" alt="" class="box__bg-img"/></div>
-                                        <div class="box__bg box__bg_hov box__bg_gray"><img src="/build/img/box__gray-hov.png" alt="" class="box__bg-img"/></div>
-                                        <div class="box__bg box__box box__bg_gray"><img src="/build/img/box__gray-box.png" alt="" class="box__bg-img"/></div>
+                                        <img  src="{{$case->image}}" alt="{{$case->name}}" title="" class="box__img__main box__img">
+                                        <div class="box__bg box__bg_hov box__bg_gray">
+                                            <img src="/build/img/box__gray-hov.png" alt="" class="box__bg-img"/>
+                                        </div>
+                                        <div class="box__bg box__box box__bg_gray">
+                                            <span class="box__price__text text-block-black">{{$case->price}} BYN</span>
+                                            <img src="/build/img/box__gray-box.png" alt="" class="box__bg-img"/>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
