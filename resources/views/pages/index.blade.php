82865bbb502828e601e3f67ee42599f35238e222
62karolina <62karolina@mail.ru>
2018-12-05 22:16:17 +0300

    дополнение к маленьким экранам

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -4,14 +4,14 @@
 <div class="cases-line">
     <div class="container">
         <div class="row cases-row box-cases-row">
-            <div class="col-md-12    col-lg-12 col-sm-12_index">
+            <div class="col-md-12 col_md-5_index col-lg-12 col-sm-12_index">
                 @foreach($cases as $case)
-                <div class="box_sm-12 box_index box_gray-box col-md-4 col-lg-4_index col-xs-12_index col-sm-12_index ">
+                <div class="box_sm-12 box_index box_gray-box col-md-4 col-lg-4_index col-xs-12_index col-sm-12_index col_md-5_index">
                     <a href="/box/{{$case->id}}">
                         <div class="row">
-                            <div class="box_sm-12 col-md-4 col-lg-4_index col-xs-12_index col-sm-12_index">
+                            <div class="box">
                                 <div class="box__bg-layout">
-                                    <div class="box__bg-wrapper">
+                                    <div class="box__bg-wrapper box_bg-wrapper_index">
                                         <img  src="{{$case->image}}" alt="{{$case->name}}" title="" class="box__img__main box__img">
                                         <div class="box__bg box__bg_hov box__bg_gray">
                                             <img src="/build/img/box__gray-hov.png" alt="" class="box__bg-img"/>
